12607de036343a690a7b95e6254d6d6b7951d273
Neubotics IT <92726406+NEUBOTICSIT@users.noreply.github.com>
2023-03-14 17:08:04 +0530

    Update settings.py

--- Personal_Info/settings.py
+++ Personal_Info/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'Personal_Info.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'personal_info',  
-        'USER': 'icai_members',  
-        'PASSWORD': 'Akshay29!',  
-        'HOST': 'database.cotbwwzlwsbz.ap-south-1.rds.amazonaws.com',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'personal_info',  
+#         'USER': 'icai_members',  
+#         'PASSWORD': 'Akshay29!',  
+#         'HOST': 'database.cotbwwzlwsbz.ap-south-1.rds.amazonaws.com',  
+#         'PORT': '3306',  
+#         'OPTIONS': {  
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+#         }  
+#     }  
 }
 
 
