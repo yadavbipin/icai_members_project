e39243410dff488be4402dbd44c11ad701e0b4ce
Neubotics IT <92726406+NEUBOTICSIT@users.noreply.github.com>
2023-03-15 17:09:14 +0530

    change database sqlite to mysql

--- Personal_Info/settings.py
+++ Personal_Info/settings.py
@@ -79,21 +79,19 @@ WSGI_APPLICATION = 'Personal_Info.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-# 'default': {  
-#         'ENGINE': 'django.db.backends.mysql',  
-#         'NAME': 'icai_members',  
-#         'USER': 'admin',  
-#         'PASSWORD': 'Akshay29!',  
-#         'HOST': 'database.cotbwwzlwsbz.ap-south-1.rds.amazonaws.com',  
-#         'PORT': '3306',  
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }  
-#     }    
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'icai_members',  
+        'USER': 'root',  
+        'PASSWORD': 'Akshay29!',  
+        'HOST': 'icaimembers.cotbwwzlwsbz.ap-south-1.rds.amazonaws.com',  
+        'PORT': '3306',  
+        
+    }      
 }
 
 
