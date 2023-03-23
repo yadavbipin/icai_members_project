7840198f858a8acfbd62aad2b8be201c4b79b760
priyanka <neuboticsitpriyanka@gmail.com>
2023-03-23 18:09:45 +0530

    change form design, add static files,changes done in views.py file

--- Info_App/views.py
+++ Info_App/views.py
@@ -14,6 +14,10 @@ from datetime import datetime
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
+#datetime field convert
+from datetime import datetime
+
+
 
 # Create your views here.
 # def index(request):  
@@ -43,30 +47,6 @@ def home(request):
     all_info=form_submission.objects.filter(member_lastname__startswith="A",holding_COP="no").order_by("member_lastname")
     context={'all_info':all_info}
     return render(request,'Info_App/home.html',context)
-
-
-# def pdf_report_create(request):   
-#     all_info=Personal_Info.objects.all()
-#     template_path = 'Info_App/pdf_report_create.html'
-#     context = {'all_info': all_info}
-#     # Create a Django response object, and specify content_type as pdf
-#     response = HttpResponse(content_type='application/pdf')
-#     # response['Content-Disposition'] = 'attachment; filename="users_report.pdf"'
-#     response['Content-Disposition'] = 'filename="users_report.pdf"'
-#     # find the template and render it.
-#     template = get_template(template_path)
-#     html = template.render(context)
-
-#     # create a pdf
-#     pisa_status = pisa.CreatePDF(
-#        html, dest=response)
-#     # if error then show some funny view
-#     if pisa_status.err:
-#        return HttpResponse('We had some errors <pre>' + html + '</pre>')
-#     return response
-
-
-
 
 
 def validate_no(request):
@@ -109,7 +89,7 @@ def validate_no(request):
         else:
             messages.error(request,"Membership Number does not exists, cannot register new member!")
             return redirect('Info_App:validate_no')
-            #  return render(request,'Info_App/submit_cainfo.html',{'membership_num':membership_num,'yr_of_membership':yr_of_membership,'Birth_date':Birth_date})
+            # return render(request,'Info_App/submit_cainfo.html',{'membership_num':membership_num,'yr_of_membership':yr_of_membership,'Birth_date':Birth_date})
     
     #Year pass to html membership year dropdown
     current_year = datetime.now().year
@@ -159,47 +139,8 @@ def submit_cainfo(request):
     return render(request,'Info_App/validate_no.html')
 
 
-# def edit_existing_cainfo(request,ca_id):
-#     fetch_info=form_submission.objects.get(id=ca_id)
-#     if request.method == "POST":
-#         member_firstname = request.POST.get('member_firstname')
-#         member_middlename = request.POST.get('member_middlename')
-#         member_lastname = request.POST.get('member_lastname')
-#         membership_number = request.POST.get('membership_number')
-#         year_of_membership = request.POST.get('year_of_membership')
-#         Phone_number = request.POST.get('Phone_number')
-#         prof_address = request.POST.get('prof_address')
-#         prof_area = request.POST.get('prof_area')
-#         prof_city = request.POST.get('prof_city')
-#         prof_pin_code = request.POST.get('prof_pin_code')
-#         residential_address = request.POST.get('residential_address')
-#         residential_area = request.POST.get('residential_area')
-#         residential_city = request.POST.get('residential_city')
-#         residential_pin_code = request.POST.get('residential_pin_code')
-#         blood_group = request.POST.get('blood_group')
-#         Date_Of_Birth = request.POST.get('DOB')
-#         Date_of_Marriage = request.POST.get('DOM')
-#         email_id = request.POST.get('email_id')
-#         organization = request.POST.get('organization')
-#         holding_COP = request.POST.get('holding_COP')   
-#         photo = request.FILES.get('photo')
-
-#         if ca_id:
-#             form_submission.objects.filter(id=ca_id).update(member_firstname=member_firstname.upper(),member_middlename=member_middlename.upper(),member_lastname=member_lastname.upper(),membership_number=membership_number,year_of_membership=year_of_membership,Phone_number=Phone_number,prof_address=prof_address,
-#                                             prof_area=prof_area,prof_city=prof_city,prof_pin_code=prof_pin_code,residential_address=residential_address,residential_area=residential_area,residential_city=residential_city,residential_pin_code=residential_pin_code,
-#                                             blood_group=blood_group,DOB=Date_Of_Birth,DOM=Date_of_Marriage,email_id=email_id,
-#                                             organization=organization,photo=photo,holding_COP=holding_COP)
-                
-#             return JsonResponse({'success': True})
-#         else:
-#             return JsonResponse({'success': False})  
-#     else:
-#         fetch_info=form_submission.objects.get(id=ca_id)
-#         return render(request,'Info_App/edit_existing_cainfo.html',{'fetch_info':fetch_info})
-
 def edit_existing_cainfo(request, ca_id):
     mymodel =form_submission.objects.get(id=ca_id)
-
     if request.method == 'POST':
         # get the new image from the request
         member_firstname = request.POST.get('member_firstname')
@@ -226,6 +167,19 @@ def edit_existing_cainfo(request, ca_id):
           new_image =mymodel.photo
         else:
           new_image = request.FILES.get('photo')
+          
+        # date field convert to yyyy-mm-dd formate
+        try:
+            date_dob = datetime.strptime(Date_Of_Birth, '%m/%d/%Y')
+        except ValueError:
+            date_dob = datetime.strptime(Date_Of_Birth, '%B %d, %Y')
+        DOB_date_str = date_dob.strftime('%Y-%m-%d')
+
+        try:
+            date_dom = datetime.strptime(Date_of_Marriage, '%m/%d/%Y')
+        except ValueError:
+            date_dom = datetime.strptime(Date_of_Marriage, '%B %d, %Y')
+        DOM_date_str = date_dom.strftime('%Y-%m-%d')
 
         # if a new image was provided, update the model's image field
         if new_image:
@@ -254,8 +208,8 @@ def edit_existing_cainfo(request, ca_id):
         mymodel.residential_city = residential_city
         mymodel.residential_pin_code = residential_pin_code
         mymodel.blood_group = blood_group
-        mymodel.DOB = Date_Of_Birth
-        mymodel.DOM = Date_of_Marriage
+        mymodel.DOB = DOB_date_str
+        mymodel.DOM = DOM_date_str
         mymodel.email_id = email_id
         mymodel.organization = organization
         mymodel.member_firstname = member_firstname
