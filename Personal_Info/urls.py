9bdaa85e1cc1a459a73bd5e91431d058387bce0e
priyanka <neuboticsitpriyanka@gmail.com>
2023-05-10 17:55:52 +0530

    add static file with out true debug

--- Personal_Info/urls.py
+++ Personal_Info/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('',include('Info_App.urls'))
 ]
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
